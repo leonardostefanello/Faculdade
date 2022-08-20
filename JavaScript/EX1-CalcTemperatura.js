a9d521a7dd2b96ac9b5e430de0713f7d9012e5a6
Leonardo Stefanello (Red) <66969944+leonardostefanello@users.noreply.github.com>
2022-08-20 18:31:37 -0300

    Update EX1-CalcTemperatura.js

--- JavaScript/EX1-CalcTemperatura.js
+++ JavaScript/EX1-CalcTemperatura.js
@@ -4,17 +4,15 @@ import java.util.Scanner;
 
 public class CalculoTemperatura {
 
-	private static final DecimalFormat df = new DecimalFormat("0.00");
-
 	public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
         
-        //Raio do Circulo
-        System.out.println("Digite o valor em fahrenheit:");
+        //Conversão
+        System.out.printf("Digite o valor em fahrenheit:");
         double fahrenheit = ler.nextDouble();
         
-        double celcius = (fahrenheit-32)*5/9;
-        System.out.println("Celcius = " + df.format(celcius));
+        double celcius = (int) (fahrenheit-32)*5/9;
+        System.out.printf("Celcius = " + celcius);
 
 	}
 
