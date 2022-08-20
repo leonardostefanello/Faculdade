c4526a1b5e3abefe87ac021b4cc4fa616c4fb08d
Leonardo Stefanello (Red) <66969944+leonardostefanello@users.noreply.github.com>
2022-08-20 20:58:18 -0300

    Create CalcSalarioLiquido2.js

---
+++ JavaScript/CalcSalarioLiquido2.js
@@ -0,0 +1,80 @@
+package exercicio1;
+
+import java.util.Scanner;
+
+public class CalcSalarioLiquido2 {
+
+	public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        
+        //Informações
+        double inss = 0;
+        double vinss = 0;
+        double irrf = 0;
+        double virrf = 0;
+        
+        System.out.println("Digite seu nome:");
+        String nome = ler.nextLine();
+
+        System.out.println("Digite o salário bruto:");
+        double sb = ler.nextInt();
+        
+        System.out.println("Digite o valor de acréscimo:");
+        double pd = ler.nextDouble();
+        
+        //Valor Adicional
+        double va = sb+pd;
+        System.out.println("Valor Adicional: " + pd);
+        
+        //Valores INSS
+        if (va <= 1212) {
+        	inss = 7.5;
+        	vinss = va*(inss/100);
+        	System.out.println("% INNS: " + inss);
+        } else if ((va <= 2427) & (va >= 1212)) {
+        	inss = 9;
+        	vinss = va*(inss/100);
+        	System.out.println("% INNS: " + inss);
+        } else if ((va <= 3641) & (va >= 2427)) {
+        	inss = 12;
+        	vinss = va*(inss/100);
+        	System.out.println("% INNS: " + inss);
+        } else if ((va <= 7087) & (va >= 3641)) {
+        	inss = 14;
+        	vinss = va*(inss/100);
+        	System.out.println("%INNS: " + inss);
+        }
+        
+        //Valores IRRF
+        if (va <= 1903) {
+        	irrf = 0;
+        	virrf = va*(irrf/100);
+        	System.out.println("% IRRF: " + irrf);
+        } else if ((va <= 2826) & (va >= 1903)) {
+        	irrf = 7.5;
+        	virrf = va*(irrf/100);
+        	System.out.println("% IRRF: " + irrf);
+        } else if ((va <= 3751) & (va >= 2826)) {
+        	irrf = 15;
+        	virrf = va*(irrf/100);
+        	System.out.println("% IRRF: " + irrf);
+        } else if ((va <= 4664) & (va >= 3751)) {
+        	irrf = 22.5;
+        	virrf = va*(irrf/100);
+        	System.out.println("% IRRF: " + irrf);
+        } else if ((va >= 4665)) {
+        	irrf = 27.5;
+        	virrf = va*(irrf/100);
+        	System.out.println("% IRRF: " + irrf);
+        }
+		
+        //Resultado
+        double vf = vinss+virrf;
+        double resultado = (va-vf);
+        		
+        System.out.println("Nome: " + nome);
+        System.out.println("Salario Liquido a receber: " + resultado);
+
+	}
+
+}
