cc85549eefdd37c4ea7e15434b639335c4e6d38d
Leonardo Stefanello (Red) <66969944+leonardostefanello@users.noreply.github.com>
2022-08-20 16:27:08 -0300

    Create Test.js

---
+++ JavaScript/Test.js
@@ -0,0 +1,11 @@
+int myInt = ler.nextInt():
+Int myInt = ler.nextInt():
+
+float myFloat = ler.nextFloat():
+Float myFloat = ler.nextFloat():
+
+double myDouble = ler.nextDouble():
+Double myDouble = ler.nextDouble():
+
+String myString = ler.nextLine():
+string myString = ler.nextLine():
