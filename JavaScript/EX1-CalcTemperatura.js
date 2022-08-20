ffa58b418f7db35380083ec125e82ad027f71c0d
Leonardo Stefanello (Red) <66969944+leonardostefanello@users.noreply.github.com>
2022-08-20 15:55:04 -0300

    Create EX1-CalcTemperatura.js

---
+++ JavaScript/EX1-CalcTemperatura.js
@@ -0,0 +1,19 @@
+package exercicio1;
+
+import java.util.Scanner;
+
+public class CalculoTemperatura {
+
+	public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        
+        //Raio do Circulo
+        System.out.println("Digite o valor em fahrenheit:");
+        float fahrenheit = ler.nextFloat();
+        
+        double celcius = (fahrenheit-32)*5/9;
+        System.out.println("Celcius = "+celcius);
+
+	}
+
+}
