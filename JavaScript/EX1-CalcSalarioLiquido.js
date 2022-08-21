be168f6cdd1ce60b57530576d1adbfab06acf1eb
Leonardo Stefanello (Red) <66969944+leonardostefanello@users.noreply.github.com>
2022-08-20 21:14:51 -0300

    Update EX1-CalcSalarioLiquido.js

--- JavaScript/EX1-CalcSalarioLiquido.js
+++ JavaScript/EX1-CalcSalarioLiquido.js
@@ -26,54 +26,47 @@ public class CalcSalarioLiquido {
         if (sb <= 1212) {
         	inss = 7.5;
         	vinss = sb*(inss/100);
-        	System.out.println("% INNS: " + inss);
         } else if ((sb <= 2427) & (sb >= 1212)) {
         	inss = 9;
         	vinss = sb*(inss/100);
-        	System.out.println("% INNS: " + inss);
         } else if ((sb <= 3641) & (sb >= 2427)) {
         	inss = 12;
         	vinss = sb*(inss/100);
-        	System.out.println("% INNS: " + inss);
         } else if ((sb <= 7087) & (sb >= 3641)) {
         	inss = 14;
         	vinss = sb*(inss/100);
-        	System.out.println("%INNS: " + inss);
         }
         
         //Valores IRRF
         if (sb <= 1903) {
         	irrf = 0;
         	virrf = sb*(irrf/100);
-        	System.out.println("% IRRF: " + irrf);
         } else if ((sb <= 2826) & (sb >= 1903)) {
         	irrf = 7.5;
         	virrf = sb*(irrf/100);
-        	System.out.println("% IRRF: " + irrf);
         } else if ((sb <= 3751) & (sb >= 2826)) {
         	irrf = 15;
         	virrf = sb*(irrf/100);
-        	System.out.println("% IRRF: " + irrf);
         } else if ((sb <= 4664) & (sb >= 3751)) {
         	irrf = 22.5;
         	virrf = sb*(irrf/100);
-        	System.out.println("% IRRF: " + irrf);
         } else if ((sb >= 4665)) {
         	irrf = 27.5;
         	virrf = sb*(irrf/100);
-        	System.out.println("% IRRF: " + irrf);
         }
         
         //Valor Desconto
         double cpd = sb*(pd/100);
-        System.out.println("% Desconto: " + cpd);
-		
+        	
         //Resultado
         double vf = vinss+virrf+cpd;
         double resultado = (sb-vf);
         		
         System.out.println("Nome: " + nome);
+	System.out.println("% Desconto: " + cpd);
         System.out.println("Salario Liquido a receber: " + resultado);
+	System.out.println("%INNS: " + inss);
+	System.out.println("% IRRF: " + irrf);
 
 	}
 
