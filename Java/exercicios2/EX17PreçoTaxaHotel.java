86f2dd5383ebb17752f53eb0c29cc79f4eb9687f
Leonardo Stefanello (Red) <66969944+leonardostefanello@users.noreply.github.com>
2022-09-13 17:16:05 -0300

    Update EX17PreçoTaxaHotel.java

--- Java/exercicios2/EX17PreçoTaxaHotel.java
+++ Java/exercicios2/EX17PreçoTaxaHotel.java
@@ -1,54 +1,52 @@
 package exercicio2;
 
-//Substitui o Scanner :)
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 import java.text.NumberFormat;
 import java.util.Locale;
 
 public class EX17PreçoTaxaHotel {
+		//cria uma função conversão() que retorna um valor já formatado com a moeda BRL (R$)
+		static String conversão(double valor) {
+			//conversão para BRL
+			Locale brazil = new Locale("pt", "BR");
+			NumberFormat nf = NumberFormat.getCurrencyInstance(brazil);
+			return nf.format(valor); //retorna o valor formatado
+		}
+
     	public static void main(String[] args) throws Exception {
-            System.out.println("------------------ INICIO ------------------");
-            System.out.println("\n");
+            System.out.println("---------------------------------------------");
             
-            //variáveis
-            String nome, dias;
-            int d2, resultado;
+            //declaração variáveis
+            Double diaria = 100.0;
+            Double tserviço = 0.0;
             
             //informações (inputs)
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            Scanner ler = new Scanner(System.in);
             System.out.println("Nome do Hospede: ");
-            nome = br.readLine();
+            String nome = ler.next();
             System.out.println("Numero de dias hospedado: ");
-            dias = br.readLine();
-            System.out.println("\n");
-            
-            d2 = Integer.parseInt(dias); //string para inteiro
-
-            //conversão para BRL
-            Locale brazil = new Locale("pt", "BR");
-            NumberFormat nf = NumberFormat.getCurrencyInstance(brazil);
+            int dias = ler.nextInt();
             
             //comparação
-            if (d2 < 10) {
-            	resultado = (100 * d2) + (8 * d2);
-                System.out.println("---------------------------------------------");
-            	System.out.println("Nome: " + nome);
-            	System.out.println("Dias Hospedado: " + d2);
-            	System.out.println("Valor a ser pago: " + nf.format(resultado));
-            } else if (d2 > 10 && d2 < 15) {
-            	resultado = (100 * d2) + (6 * d2);
-                System.out.println("---------------------------------------------");
-            	System.out.println("Nome: " + nome);
-            	System.out.println("Dias Hospedado: " + d2);
-            	System.out.println("Valor a ser pago: " + nf.format(resultado));
-            } else if (d2 > 15) {
-            	resultado = (100 * d2) + (5 * d2);
-                System.out.println("---------------------------------------------");
-            	System.out.println("Nome: " + nome);
-            	System.out.println("Dias Hospedado: " + d2);
-            	System.out.println("Valor a ser pago: " + nf.format(resultado));
+            if (dias < 10) {
+            	tserviço = 8.0;
+            } else if (dias >= 10 && dias >= 14) {
+            	tserviço = 6.0;
+            } else if (dias > 15) {
+            	tserviço = 5.0;
             }
+
+            //calculos
+            Double resultado = diaria * dias + tserviço;
+
+            //print
             System.out.println("---------------------------------------------");
+            System.out.println("Nome: " + nome);
+            System.out.println("Dias Hospedado: " + dias);
+            System.out.println("Valor a ser pago: " + conversão(resultado));
+            System.out.println("---------------------------------------------");
+
+            //fechar scanner
+            ler.close();
     }
 }
